314a8c1cc67c25e93d4d9779c2320456e1e66e4a
idrmiller <idrmiller360@gmail.com>
2016-01-10 18:10:26 -0500

    completed required functionality

--- ColorMaker_Slider/ViewController.swift
+++ ColorMaker_Slider/ViewController.swift
@@ -9,15 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redText: UITextField!
+    @IBOutlet weak var greenText: UITextField!
+    @IBOutlet weak var blueText: UITextField!
+    
+    @IBOutlet weak var colorView: UIView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        pickColor()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func pickColor() {
+        
+        redText.text = String(Int(redSlider.value))
+        greenText.text = String(Int(greenSlider.value))
+        blueText.text = String(Int(blueSlider.value))
+        
+        let r: CGFloat = CGFloat(redSlider.value/255)
+        let g: CGFloat = CGFloat(greenSlider.value/255)
+        let b: CGFloat = CGFloat(blueSlider.value/255)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        
+        
     }
 
 
