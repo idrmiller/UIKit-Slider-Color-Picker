157f2ac49b7a5084d0df96f26a9c08608679e697
idrmiller <idrmiller360@gmail.com>
2016-01-10 14:06:56 -0500

    Initial Commit

---
+++ ColorMaker_SliderTests/ColorMaker_SliderTests.swift
@@ -0,0 +1,36 @@
+//
+//  ColorMaker_SliderTests.swift
+//  ColorMaker_SliderTests
+//
+//  Created by David Miller on 1/10/16.
+//  Copyright © 2016 LM Labs. All rights reserved.
+//
+
+import XCTest
+@testable import ColorMaker_Slider
+
+class ColorMaker_SliderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
